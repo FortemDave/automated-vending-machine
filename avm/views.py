01cd96cbcf13b0b4d242ef59c37e1707ac3dd4fb
Prateek Agrawal <20bcs101@iiitdwd.ac.in>
2021-11-20 17:16:05 +0530

    number

--- avm/views.py
+++ avm/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .models import Item, order
 import json
 import sqlite3
+import random
+import requests
+
 # Create your views here.
 
 def index(request):
@@ -52,14 +55,74 @@ def checkout(request):
 
             db.commit()
 
+        # Using the website to execute API commands
+        url = "https://www.fast2sms.com/dev/bulk"
+
+        #Sending a 4 digit verification number, use it for authentication
+        verification_code = random.randint(1000,9999)
+
+
         phoneNo = request.POST.get('phone','')
+        def valindIndianNum(number):
+            if len(number) != 10:
+                return 0
+            elif number[0:3] == "040":
+                #IF A LANDLINE NUMBER IS GIVEN
+                return 0
+            elif number[0] not in ["6","7","8","9"]:
+                #Non INDIAN number
+                return 0
+            return 1
+
+        # Take the input here, you decide how
+        print(phoneNo)
+        indian_number = str(phoneNo)
+        print(type(phoneNo))
+        indian_number = indian_number.strip()
+
+        message = "Your AVM OTP is : {a}. If you did not intend to recieve this message, kindly ignore it.".format(a = verification_code)
+
+        # API SPECIFIC INFORMATION--------------------------------
+        my_data = {
+            'sender_id': 'FTSMS',
+            'message': message,
+            'language': 'english',
+            'route':'p',
+            'numbers': '{}'.format(indian_number)
+        }
+
+        # DO NOT MODIFY or SHARE this data WITH ANYONE, contains personal information
+        # of a colleage. 
+        headers = {
+            'authorization': 'GbwHQzC2yWKlPLajAUOxFc6ZSknTprMthNBmJgER903v5fos8ibB8PIhDVXefSqCunL3EpxZUGtWQkl6',
+            'Content-Type': "application/x-www-form-urlencoded",
+            'Cache-Control': "no-cache"
+        }
+        #---------------------------------------------------------
+
+        if(valindIndianNum(phoneNo)):
+            response = requests.request("POST",
+                                        url,
+                                        data = my_data,
+                                        headers = headers)
+
+            returned_msg = json.loads(response.text)
+        
+            # Uncomment if you want to see the transaction status
+            # print(returned_msg['message'])
+        else:
+            # Do what you want to, dummy code here
+            print("INVALID NUMBER")
+
+        print(verification_code)
         orders =  order(items_json = itemJson, phone_no = phoneNo)
         orders.save()
         thank = True
         id = orders.id
         context = {
             'thank' : thank,
-            'id':id
+            'id':id,
+            'code':verification_code
         }
         # print(keys)
         return render(request, 'avm/checkout.html', context)
